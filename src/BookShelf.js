9ecb025b177f2c8e046710115af795633fc29b73
Casey Cole <caseycole589@gmail.com>
2018-10-01 18:54:32 -0400

    Feature: Users Can Now Navigate Between Search And Main View Plus Move Books To Different Shelves

---
+++ src/BookShelf.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Book from './Book'
+const BookShelf = props => {
+    const { shelfFitler, title, books, onUpdateBook } = props;
+    const filteredBooks = books.filter(book => book.shelf === shelfFitler);
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {filteredBooks.map(book => (
+                        <Book onUpdateBook={onUpdateBook} key={book.id} book={book} />
+                    ))}
+                </ol>
+            </div>
+        </div>
+    );
+}
+export default BookShelf;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import BookShelf from './BookShelf'
+import { Link } from "react-router-dom";
 const ListBooks = props => {
-    const { books } = props;
+    const { books, onUpdateBook } = props;
     return (
-        <div>
+        <div className="list-books">
             <div className="list-books-title">
                 <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
                 <div>
-                    <BookShelf books={books} title={"Currently Reading"} shelfFitler={'currentlyReading'} />
-                    <BookShelf books={books} title={"Want to Read"} shelfFitler={'wantToRead'} />
-                    <BookShelf books={books} title={"Want to Read"} shelfFitler={'read'} />
+                    <BookShelf onUpdateBook={onUpdateBook} books={books} title={"Currently Reading"} shelfFitler={'currentlyReading'} />
+                    <BookShelf onUpdateBook={onUpdateBook} books={books} title={"Want to Read"} shelfFitler={'wantToRead'} />
+                    <BookShelf onUpdateBook={onUpdateBook} books={books} title={"Read"} shelfFitler={'read'} />
                 </div>
+            </div>
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
             </div>
         </div>
     );
