6c4c0340ef36fe77c8292418fdc7d1d34adae622
Casey Cole <caseycole589@gmail.com>
2018-10-02 13:30:16 -0400

    fix: Books Without Images No Longer Throw Errors

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ const Book = props => {
         <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
                         <select value={book.shelf} onChange={handleSelect}>
                             <option value="move" disabled>Move to...</option>
